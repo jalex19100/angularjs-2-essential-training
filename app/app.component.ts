760cb7ca31cf3a9c7e81cddb198d1738fe502662
Neandher Carlos <neandher89@gmail.com>
2016-05-21 20:38:51 -0300

    Course Done!

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,17 @@
 import {Component} from 'angular2/core';
+import {MediaItemListComponent} from './media-item-list.component';
+import {MediaItemFormComponent} from './media-item-form.component';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
+@RouteConfig([
+    {path: '/:medium', component: MediaItemListComponent, name: 'List'},
+    {path: '/add', component: MediaItemFormComponent, name: 'AddMediaItem'}
+])
 @Component({
     selector: 'media-tracker-app',
-    template: '<h1>My App</h1>'
+    directives: [ROUTER_DIRECTIVES],
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+}
